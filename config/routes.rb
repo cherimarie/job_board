0b58938fc547c9464319d4ace9fca98652b9b369
cheri allen <cherimarie@gmail.com>
2014-01-31 11:28:38 -0800

    Begin listings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 JobBoard::Application.routes.draw do
 
   resources :listings
-  
+
+  root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
