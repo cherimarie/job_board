a686da3794234a3148a4e4036d503c428348451b
cheri allen <cherimarie@gmail.com>
2014-02-14 11:39:26 -0800

    Only serve up listings that are aprroved within 30 days

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,7 +4,19 @@ class ListingsController < ApplicationController
   before_filter :prepare_categories
 
   def index
-    @listings = Listing.all
+    #@listings = Listing.all
+    
+    # Listing.find_each do |l|
+    #   if l.approved = true 
+    #     if l.date_approved > DateTime.now.to_date-30.days
+    #       @listings.push l
+    #     end
+    #   end 
+    # end 
+
+
+    @listings = Listing.where("approved = ? AND date_approved > ?", true, DateTime.now.to_date-30.days)
+
   end 
 
   def search
