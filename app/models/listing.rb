cd86851329f349491b1ffd93061642450c43ed9b
cheri allen <cherimarie@gmail.com>
2014-02-14 10:31:40 -0800

    Add validations to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,15 @@
 class Listing < ActiveRecord::Base
   belongs_to :category
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :headquarters, presence: true
+  validates :app_instructions, presence: true
+  validates :company_name, presence: true
+  validates :company_url, presence: true
+  validates :company_email, presence: true
+  validates :category_id, presence: true
+
   def self.search(search)
     if search
       find(:all, :conditions => ['title LIKE ? OR description LIKE ? OR company_name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"] )
