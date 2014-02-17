146f72ea24bed1699f8e2ad219149885cf7eb62f
cheri allen <cherimarie@gmail.com>
2014-02-17 08:36:51 -0800

    User creation flow working well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, notice: "Admin created."
+      redirect_to users_path, notice: "Admin created."
     else
       render "new"
     end
   end
+
+  def index 
+    @users = User.all
+  end 
+
+
 end
