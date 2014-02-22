0ad867c52c3d6640492d8a2fdfc7da970cee67e3
cheri allen <cherimarie@gmail.com>
2014-02-22 12:55:55 -0800

    Users are automatically logged in when they change their password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -21,6 +21,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 24.hours.ago
       redirect_to new_password_reset_path, alert: "Password reset has expired."
     elsif @user.update_attributes(user_params)
+      session[:user_id] = @user.id 
       redirect_to admin_path, notice: "Password has been reset."
     else
       render :edit
