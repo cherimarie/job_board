1136a55c6b58746cff41701032c6112a2f0e27d1
cheri allen <cherimarie@gmail.com>
2014-02-22 13:55:01 -0800

    Adding configs for Heroku

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -15,9 +15,8 @@ class LogoUploader < CarrierWave::Uploader::Base
   process :resize_to_fit => [150, 150]
 
   # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
-     %w(jpg jpeg gif png)
+     %w(jpg jpeg gif png bmp)
   end
 
 end
