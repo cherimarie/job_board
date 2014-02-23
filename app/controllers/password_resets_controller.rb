480498464815470622390919cc2460d3de203bbb
cheri allen <cherimarie@gmail.com>
2014-02-23 13:40:07 -0800

    Password reset tokens only good for single use

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,7 +11,12 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user = User.find_by_password_reset_token!(params[:id])
+    @user = User.find_by_password_reset_token(params[:id])
+
+    if @user.nil?
+      redirect_to login_path
+      flash[:error] = "Please try again."
+    end 
   end
 
   def update
@@ -22,6 +27,7 @@ class PasswordResetsController < ApplicationController
       redirect_to new_password_reset_path, alert: "Password reset has expired."
     elsif @user.update_attributes(user_params)
       session[:user_id] = @user.id 
+      @user.update(password_reset_token: nil)
       redirect_to admin_path, notice: "Password has been reset."
     else
       render :edit
