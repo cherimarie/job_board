6006b82a7b88e8025cf20c0261aa268e83ed611c
cheri allen <cherimarie@gmail.com>
2014-02-22 17:16:16 -0800

    Tidying up

--- app/mailers/submitter_mailer.rb
+++ app/mailers/submitter_mailer.rb
@@ -3,7 +3,6 @@ class SubmitterMailer < ActionMailer::Base
 
   def payment_instructions(listing)
     @listing = listing
-
     mail to: listing.company_email, subject: "Your listing on DevLocal Jobs"
   end
 end
